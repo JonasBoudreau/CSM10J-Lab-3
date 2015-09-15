07b3f903b890f3273df6e2f94e1d1bf56f3bcf7e
Jonas Boudreau <jonasboudreau@gmail.com>
2015-09-15 14:44:38 -0700

    Create Lab03_B_DoWhile.java

---
+++ Lab03_B_DoWhile.java
@@ -0,0 +1,40 @@
+package lab03_b_dowhile;
+import java.util.*;
+
+/**
+ * Jonas Boudreau
+ * CSM10J Lab03_B_DoWhile
+ */
+public class Lab03_B_DoWhile {
+    
+    static Scanner console = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        int num1, num2, counter, sum = 0;
+        System.out.print("Input the first number: ");
+        num1 = console.nextInt();
+        System.out.print("Input the second number: ");
+        num2 = console.nextInt();
+        
+        System.out.println("\nAll odd numbers between " 
+                + num1 + " and " + num2 + " inclusive.");
+
+        counter = num1;
+        do {
+            if (counter % 2 != 0){
+                System.out.println(counter);
+            }
+            else {
+                sum += counter;
+            }
+            counter++;
+        }
+        while (counter <= num2);
+        
+        System.out.println("\nSum of all even numbers between " 
+                + num1 + " and " + num2 + " inclusive");
+        System.out.println(sum);
+    }
+    
+}
